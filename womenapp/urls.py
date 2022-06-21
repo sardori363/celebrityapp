790b560f106e3750c20618dac971260ba6645194
Sardorbek Odiljonov <sardorbekodiljonov363@gmai.com>
2022-06-21 20:11:04 +0500

    initial commit

---
+++ womenapp/urls.py
@@ -0,0 +1,17 @@
+from codecs import register_error
+from django.urls import path, re_path
+from django.views.decorators.cache import cache_page
+
+from .views import *
+
+urlpatterns = [
+    path('',WomenHome.as_view(), name='home'),
+    path('about/', about, name='about'),
+    path('addpage/', AddPage.as_view(), name='add_page'),
+    path('contact/', ContactFormView.as_view(), name='contact'),
+    path('login/', LoginUser.as_view(), name='login'),
+    path('register/', RegisterUser.as_view(), name='register'),
+    path('logout/', logout_user, name='logout'),
+    path('post/<slug:post_slug>/', ShowPost.as_view(), name='post'),
+    path('category/<slug:cat_slug>/', WomenCategory.as_view(), name='category'),
+]
